de2fff87302ee59a972fdc6f0914fa9a730fd6d1
Max <maxim.macari.dam@gmail.com>
2020-10-28 13:11:25 +0100

    Initial Commit

---
+++ GradientPicker/GradientPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientPickerApp.swift
+//  GradientPicker
+//
+//  Created by Maxim Macari on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GradientPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
